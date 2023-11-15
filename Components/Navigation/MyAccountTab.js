c2154a62ea296c7b9e76a81b1fddfe05d9509116
Saad Qureshi <saad-qureshi2001@outlook.com>
2023-11-15 14:03:54 +0000

    Achievement Styling added

--- Components/Navigation/MyAccountTab.js
+++ Components/Navigation/MyAccountTab.js
@@ -1,23 +1,37 @@
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import UserDetails from '../MyAccount/UserDetails';
-import AchievmentButton from '../MyAccount/AchievmentButton';
+import AchievementButton from '../MyAccount/AchievementButton';
 import LocationHistoryButton from '../MyAccount/LocationHistoryButton';
 import AuthDetails from './AccountSetup/Auth';
 import { useUser } from './AccountSetup/UserContext';
 
 export default function MyAccountTab({navigation}) {
-  const user = useUser()
-    return (
-      <View 
-      style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
-      >
-        
-        <Text>Welcome {user.displayName}</Text>
-        <UserDetails />
-        <AchievmentButton navigation={navigation}/>
-        <LocationHistoryButton navigation={navigation}/>
-        <AuthDetails/>
+  const user = useUser();
 
-      </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text style={styles.welcomeText}> Welcome </Text>
+      <UserDetails />
+      <AchievementButton navigation={navigation} user={user}/>
+      <LocationHistoryButton navigation={navigation}/>
+      <AuthDetails/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 10,
+    backgroundColor: '#D0e5ec', 
+    height: 100
+  },
+  welcomeText: {
+    fontSize: 22,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#00235A', 
   }
+});
