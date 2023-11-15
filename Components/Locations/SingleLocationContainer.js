de6946d82712f68540aa13ff22c4ce8869e8c85e
Meron G <merong@Merons-MacBook-Air.local>
2023-11-15 16:08:46 +0000

    loading screen

--- Components/Locations/SingleLocationContainer.js
+++ Components/Locations/SingleLocationContainer.js
@@ -115,7 +115,7 @@ const styles = StyleSheet.create({
   },
   directionsShare: {
     position: 'absolute',
-    bottom: 250,
+    top: 135,
     left: 14
   },
   navigationButton: {
--- Components/Locations/TopLocations.js
+++ Components/Locations/TopLocations.js
@@ -6,50 +6,8 @@ import { ScrollView } from 'react-native-gesture-handler';
 import { getDistance } from 'geolib'
 
 
-export default function TopLocations({navigation, userLocation}) {
-
-const [topRatedLocations, setTopRatedLocations] = useState([])
-const [loadingTopRatedLocations, setLoadingTopRatedLocations] = useState(true)
-
-const userLatitude = userLocation.coords.latitude
-const userLongitude = userLocation.coords.longitude
-
-useEffect(()=>{
-  getLocations()
-  .then(({locations})=>{
-
-
-    const allLocationsDistance = locations.map((location)=>{
-
-      const locationCopy = {...location}
-
-      const stringLocation = JSON.stringify(locationCopy)
-      const parsedLocation = JSON.parse(stringLocation)
-
-    locationCopy.distance = getDistance({latitude: userLatitude, longitude : userLongitude},{latitude: parsedLocation.coordinates[1], longitude : parsedLocation.coordinates[0]})
-
-    return locationCopy
-
-    })
-
-    const sortedTopLocations = allLocationsDistance.sort((a,b)=>a.avg_rating - b.avg_rating)
-
-    setTopRatedLocations(sortedTopLocations.slice(-20).reverse())
-    setLoadingTopRatedLocations(false)
-
-
-  })
-    },[])
-
-    if (loadingTopRatedLocations) {
-      return (
-        <View>
-          <Text>Loading...</Text>
-        </View>
-      )
-    } 
-
-
+export default function TopLocations({navigation, topRatedLocations}) {
+console.log(topRatedLocations);
     return (
       <ScrollView horizontal={true} style={{ flexDirection: 'row', margin: 10}}>
         {topRatedLocations.map((location)=> {
