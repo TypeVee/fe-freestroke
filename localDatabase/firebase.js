66d8d9593307af48cfffccf0f8faca2f2cf92d98
Meron G <merong@Merons-MacBook-Air.local>
2023-11-16 12:46:15 +0000

    fixed postlocation

--- localDatabase/firebase.js
+++ localDatabase/firebase.js
@@ -1,24 +1,27 @@
-import { initializeApp } from 'firebase/app';
-import { getAuth, initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  initializeAuth,
+  getReactNativePersistence,
+} from "firebase/auth";
 import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { getFirestore } from 'firebase/firestore'
-import 'react-native-get-random-values';
-import { firebaseConfig } from './fbconfig';
-import AsyncStorage from '@react-native-async-storage/async-storage';  
-
+import { getFirestore } from "firebase/firestore";
+import "react-native-get-random-values";
+import { firebaseConfig } from "./fbconfig";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { v4 as uuidv4 } from "uuid";
 
 const app = initializeApp(firebaseConfig);
 const auth = initializeAuth(app, {
   persistence: getReactNativePersistence(AsyncStorage),
 });
 
-const db = getFirestore(app)
-
+const db = getFirestore(app);
 
 async function handleImagePicked(ImageUri) {
-        const uploadUrl = await uploadImageAsync(ImageUri);
-        return uploadUrl
-  }
+  const uploadUrl = await uploadImageAsync(ImageUri);
+  return uploadUrl;
+}
 
 async function uploadImageAsync(uri) {
   const blob = await new Promise((resolve, reject) => {
@@ -34,22 +37,23 @@ async function uploadImageAsync(uri) {
     xhr.open("GET", uri, true);
     xhr.send(null);
   });
-    const fileRef = ref(getStorage(), uuid.v4());
-    const result = await uploadBytes(fileRef, blob);
-    blob.close();
-  
-    return await getDownloadURL(fileRef);
-  }
+  const fileRef = ref(getStorage(), uuidv4());
+  const result = await uploadBytes(fileRef, blob);
+  blob.close();
 
-const uploadImage = async function (ImageUri){
-      return new Promise((resolve, reject)=> handleImagePicked(ImageUri)
-      .then((url)=>{
-        console.log(url, '<<<url')
-        resolve(url)}))
-      .catch((error) => {
-        console.log('Error handling image:', error);
-        reject(error);
-      });
-    }
+  return await getDownloadURL(fileRef);
+}
 
-module.exports = { initializeApp, uploadImage, auth, db}
+const uploadImage = async function (ImageUri) {
+  return new Promise((resolve, reject) =>
+    handleImagePicked(ImageUri).then((url) => {
+      console.log(url, "<<<url");
+      resolve(url);
+    })
+  ).catch((error) => {
+    console.log("Error handling image:", error);
+    reject(error);
+  });
+};
+
+module.exports = { initializeApp, uploadImage, auth, db };
